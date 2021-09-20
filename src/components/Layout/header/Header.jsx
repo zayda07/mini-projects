0808de707dbe80cd15ea8550174f7926a9cab73f
Zayda Rivera <zayda07@misena.edu.co>
2021-09-20 05:46:45 -0500

    cambios

--- src/components/Layout/header/Header.jsx
+++ src/components/Layout/header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Header.css";
-
+import Logo from "../../assets/images/logo.png";
 const Header = () => {
     return (
         <div className="headerSection">
+            <div className="img">
+                <img src={Logo} alt={"logoimage"} />
+            </div>
             <div className="title">
                 <p>25 Miniproyectos usando HTML, CSS y javascript</p>
             </div>
--- src/retosList.js
+++ src/retosList.js
@@ -14,36 +14,46 @@ import progressSteps from "../src/components/assets/images/progressSteps.png";
 import rotatingNavigation from "../src/components/assets/images/rotatingNavigation.png";
 import scrollAnimation from "../src/components/assets/images/scrollAnimation.png";
 import soundBoard from "../src/components/assets/images/soundBoard.png";
+import netflixNavigation from "../src/components/assets/images/netflixNavigation.png";
+import testimonialBoxSwitcher from "../src/components/assets/images/testimonialBoxSwitcher.png";
+import pokedexAPI from "../src/components/assets/images/pokedexAPI.png";
+import dobleClickHeart from "../src/components/assets/images/dobleClickHeart.png";
+import eventKeycodes from "../src/components/assets/images/eventKeycodes.png";
+import mobileNavNavigation from "../src/components/assets/images/mobileNavNavigation.png";
+import drinkWater from "../src/components/assets/images/drinkWater.png";
+import feedbackUIDesign from "../src/components/assets/images/feedbackUIDesign.png";
+import githubProfile from "../src/components/assets/images/githubProfile.png";
+
 const retos = [
     {
         id: 1,
-        image: autoText,
-        title: "Auto text",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%207%20-%20AutoText/index.html",
+        image: expandingCards,
+        title: "Expanding Cards",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%201%20-%20Expanding%20Cards/index.html",
     },
     {
         id: 2,
-        image: backgroundSlider,
-        title: "Background slider",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%2015%20-%20Background%20Slider/index.html",
+        image: progressSteps,
+        title: "Progress steps",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%202%20-%20Progress%20Steps/index.html",
     },
     {
         id: 3,
-        image: blurryLoading,
-        title: "Blurry Loading",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%205%20-%20Blurry%20Loading/index.html",
+        image: rotatingNavigation,
+        title: "Rotating Navigation",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%203%20-%20Rotating%20Navigation/index.html",
     },
     {
         id: 4,
-        image: buttonRippleEffect,
-        title: "Button Ripple Effect",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%2014%20-%20Button%20Ripple%20Effect/index.html",
+        image: hiddenSearch,
+        title: "Hidden search",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%204%20-%20Hidden%20Search/index.html",
     },
     {
         id: 5,
-        image: dadJokes,
-        title: "Dad Jokes",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%2011%20-%20Dad%20Jokes/index.html",
+        image: blurryLoading,
+        title: "Blurry Loading",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%205%20-%20Blurry%20Loading/index.html",
     },
     {
         id: 6,
@@ -53,63 +63,117 @@ const retos = [
     },
     {
         id: 7,
-        image: expandingCards,
-        title: "Expanding Cards",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%201%20-%20Expanding%20Cards/index.html",
+        image: autoText,
+        title: "Auto text",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%207%20-%20AutoText/index.html",
     },
     {
         id: 8,
-        image: formInputWave,
-        title: "Form input wave",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%2010%20-%20Form%20input%20wave/index.html",
-    },
-    {
-        id: 9,
-        image: hiddenSearch,
-        title: "Hidden search",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%204%20-%20Hidden%20Search/index.html",
-    },
-    {
-        id: 10,
         image: hoverBoar,
         title: "Hover boar",
         url: "https://zayda07.github.io/retos/D%C3%ADa%208%20-%20HoverBoard/index.html",
     },
     {
-        id: 11,
-        image: kineticLoader,
-        title: "Kinetic loader",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%2016%20-%20Kinetic%20loader/index.html",
-    },
-    {
-        id: 12,
-        image: liveUserFilter,
-        title: "Live user filter",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%2013%20-%20Live%20User%20Filter/index.html",
-    },
-    {
-        id: 13,
-        image: progressSteps,
-        title: "Progress steps",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%202%20-%20Progress%20Steps/index.html",
-    },
-    {
-        id: 14,
-        image: rotatingNavigation,
-        title: "Rotating Navigation",
-        url: "https://zayda07.github.io/retos/D%C3%ADa%203%20-%20Rotating%20Navigation/index.html",
-    },
-    {
-        id: 15,
+        id: 9,
         image: scrollAnimation,
         title: "Scroll animation",
         url: "https://zayda07.github.io/retos/D%C3%ADa%209%20-%20Scroll%20Animation/index.html",
     },
     {
-        id: 16,
+        id: 10,
+        image: formInputWave,
+        title: "Form input wave",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2010%20-%20Form%20input%20wave/index.html",
+    },
+    {
+        id: 11,
+        image: dadJokes,
+        title: "Dad Jokes",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2011%20-%20Dad%20Jokes/index.html",
+    },
+    {
+        id: 12,
         image: soundBoard,
         title: "Sound Board",
         url: "https://zayda07.github.io/retos/D%C3%ADa%2012%20-%20Sound%20Board/index.html",
+    },
+    {
+        id: 13,
+        image: liveUserFilter,
+        title: "Live user filter",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2013%20-%20Live%20User%20Filter/index.html",
+    },
+    {
+        id: 14,
+        image: buttonRippleEffect,
+        title: "Button Ripple Effect",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2014%20-%20Button%20Ripple%20Effect/index.html",
+    },
+    {
+        id: 15,
+        image: backgroundSlider,
+        title: "Background slider",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2015%20-%20Background%20Slider/index.html",
+    },
+    {
+        id: 16,
+        image: kineticLoader,
+        title: "Kinetic loader",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2016%20-%20Kinetic%20loader/index.html",
+    },
+    {
+        id: 17,
+        image: netflixNavigation,
+        title: "Netflix Navigation",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2017%20-%20Netflix%20Navigation/",
+    },
+    {
+        id: 18,
+        image: testimonialBoxSwitcher,
+        title: "Testimonial Box Switcher",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2018%20-%20Testimonial%20Box%20Switcher/",
+    },
+    {
+        id: 19,
+        image: pokedexAPI,
+        title: "Pokedex API",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2019%20-%20Pokedex%20API/",
+    },
+    {
+        id: 20,
+        image: dobleClickHeart,
+        title: "Doble Click Heart",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2020%20-%20Doble%20click%20Heart/",
+    },
+    {
+        id: 21,
+        image: eventKeycodes,
+        title: "Event Keycodes",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2021%20-%20Event%20keycodes/",
+    },
+    {
+        id: 22,
+        image: mobileNavNavigation,
+        title: "Mobile Nav Navigation",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2022%20-%20Mobile%20Nav%20Navigation/",
+    },
+    {
+        id: 23,
+        image: drinkWater,
+        title: "Drink Water",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2023%20-%20Drink%20Water/",
+    },
+    {
+        id: 24,
+        image: feedbackUIDesign,
+        title: "Feedback UI Design",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2024%20-%20Feedback%20UI%20Design/",
+    },
+    {
+        id: 25,
+        image: githubProfile,
+        title: "Github Profile",
+        url: "https://zayda07.github.io/retos/D%C3%ADa%2025%20-%20Github%20Profile/",
     },
 ];
 
